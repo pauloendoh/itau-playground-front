31e0df3f5d2245a3d45de495fe03ebe7740e2b72
Paulo Endoh <paulo.endoh@gmail.com>
2026-07-13 00:06:20 -0300

    reverte para usar o typedRow simples

--- src/app/components/pages/logged-page/logged-page.component.ts
+++ src/app/components/pages/logged-page/logged-page.component.ts
@@ -1,17 +1,10 @@
-import {
-  Component,
-  computed,
-  inject,
-  TemplateRef,
-  viewChild,
-} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 import { FormularioHttpClient } from '../../../http-clients/formulario/formulario.http-client';
 import { FormularioResponseDto } from '../../../http-clients/formulario/types/formulario-response.dto';
-import { MyColumnDefs } from '../../../utils/types/my-column-def';
-import { MyMatTableComponent } from '../../_shared/my-mat-table/my-mat-table-component';
 import { NovaSolicitacaoButtonComponent } from './nova-solicitacao-button/nova-solicitacao-button.component';
 
 @Component({
@@ -20,44 +13,26 @@ import { NovaSolicitacaoButtonComponent } from './nova-solicitacao-button/nova-s
   imports: [
     MatCardModule,
     MatButtonModule,
+    MatTableModule,
     NovaSolicitacaoButtonComponent,
-    MyMatTableComponent,
   ],
   templateUrl: './logged-page.component.html',
-  styleUrls: ['./logged-page.component.scss'],
+  styleUrl: './logged-page.component.scss',
 })
 export class LoggedPageComponent {
   private formularioHttpClient = inject(FormularioHttpClient);
 
   formularios: FormularioResponseDto[] = [];
+  displayedColumns = [
+    'codFormulario',
+    'tipoFormulario',
+    'situacaoFormulario',
+    'codigoCar',
+  ];
 
-  situacaoCell =
-    viewChild<TemplateRef<{ $implicit: FormularioResponseDto }>>(
-      'situacaoCell',
-    );
-
-  columnDefs = computed<MyColumnDefs<FormularioResponseDto>>(() => [
-    {
-      columnId: 'codFormulario',
-      headerLabel: 'Nº Solicitação',
-      cellText: (item) => item.codFormulario,
-    },
-    {
-      columnId: 'tipoFormulario',
-      headerLabel: 'Tipo Formulário',
-      cellText: (item) => item.tipoFormulario,
-    },
-    {
-      columnId: 'situacaoFormulario',
-      headerLabel: 'Situação',
-      cellTemplate: this.situacaoCell()!,
-    },
-    {
-      columnId: 'codigoCar',
-      headerLabel: 'CAR',
-      cellText: (item) => item.codigoCar ?? '-',
-    },
-  ]);
+  protected typedRow(row: unknown): FormularioResponseDto {
+    return row as FormularioResponseDto;
+  }
 
   constructor() {
     this.formularioHttpClient
--- src/examples.ts
+++
@@ -1,3 +0,0 @@
-import { LoggedPageComponent } from './app/components/pages/logged-page/logged-page.component';
-
-LoggedPageComponent; // componente com tabela mat-table
